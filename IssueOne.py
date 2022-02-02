41c0ff5ec9662c8a0b1efa57f151d243c4db751c
Abbey Noyes <abbeynoyes@gmail.com>
2022-02-02 16:44:13 -0500

    QuickUpdate
    Testing

---
+++ IssueOne.py
@@ -0,0 +1,9 @@
+#Revision #1 Begin Date: 2/2/2022
+#Begin Abigail Noyes 2/2/2020
+
+class Number:
+    
+
+
+# Revision #1 End Date:
+#Group 7 / Manager : Mina Stanton / Lead Teach: Abigail Noyes / Project #1
