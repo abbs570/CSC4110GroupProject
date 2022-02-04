c069d42e6706ea34b04eca01c85b12abf6810a4b
Abbey Noyes <abbeynoyes@gmail.com>
2022-02-04 15:46:08 -0500

    Updates

---
+++ Issue5.py
@@ -0,0 +1,23 @@
+#Revision #1 Begin Date: 2/4/2022
+#Begin Abigail Noyes 2/2/2020
+
+import random
+class PiratesLife:
+
+
+    treasure = {}
+    while True:
+        item = input("What would you like to add to the treasure chest?: ")
+        numberOfItems = input("How many" + item + "like to add to the treasure chest?: " )
+        addingItems = input("Would you like to add more items? (type 'y' for yes and 'n' for no) : ")
+        if addingItems == 'n':
+            break
+        
+
+
+
+
+
+#Revision #1 End Date: 2/4/2022
+#End Abigail Noyes / Project #1
+#Manager:Abigail Noyes / Lead Tech: Abigail Noyes / Project #3
---
+++ IssueOne.py
@@ -0,0 +1,13 @@
+#Revision #3 Begin Date: 2/2/2022
+#Begin Abigail Noyes 2/2/2020
+
+
+#takes number from user
+number = int(input("Enter a number: ")) 
+#number + 2 * 3 - 6 / 3
+#should print the same number
+print (((number + 2) * 3 - 6) / 3) 
+    
+#Revision #3 End Date: 2/4/2022
+#End Abigail Noyes / Project #1
+#Manager:Abigail Noyes / Lead Tech: Abigail Noyes / Project #3
