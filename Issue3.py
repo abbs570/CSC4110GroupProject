775238231f21c6956fedd8cd83aad959cc3c11cf
Abbey Noyes <abbeynoyes@gmail.com>
2022-02-04 14:12:43 -0500

    Update

--- Issue3.py
+++ Issue3.py
@@ -1,6 +1,15 @@
 #Revision #1 Begin Date: 2/4/2022
 #Begin Abigail Noyes 2/2/2020
 
+userinput = input("Input a number: ") #user input
+
+print ("print as string value: " + str(userinput))    #prints as string value
+
+print("print as integer value: "+ int(userinput))  #prints as integer value
+
+print("print as float- point value: "+ userinput)  #prints as float point value
+
+
 
 
 #Revision #1 End Date: 2/4/2022
---
+++ Issue4.py
@@ -0,0 +1,16 @@
+#Revision #1 Begin Date: 2/4/2022
+#Begin Abigail Noyes 2/2/2020
+
+
+expressionA = (2**2**3) #expression a
+expressionB =(2**(2**3))#expression b
+expressionC =((2**2)**3)#expression c
+
+if (expressionA == expressionB and expressionB == expressionC): #test if outputs are equal
+    print("The expressions are the same")
+
+else: #print if they are not equal
+    print("The expressions are different")
+
+#Revision #1 Begin Date: 2/4/2022
+#Begin Abigail Noyes 2/2/2020
