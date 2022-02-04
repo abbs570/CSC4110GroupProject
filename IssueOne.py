d65193b4fd19801327f850e9fda0c57e696b38ea
Abbey Noyes <abbeynoyes@gmail.com>
2022-02-04 12:37:53 -0500

    Quick Update
    Revision #3

--- IssueOne.py
+++ IssueOne.py
@@ -1,11 +1,12 @@
-#Revision #1 Begin Date: 2/2/2022
+#Revision #3 Begin Date: 2/2/2022
 #Begin Abigail Noyes 2/2/2020
 
 class Number:
 
-    number = int(input("Enter a number: "))
-    print (((number + 2) * 3 - 6) / 3)
+    number = int(input("Enter a number: ")) #takes number from user
+    #number + 2 * 3 - 6 / 3
+    print (((number + 2) * 3 - 6) / 3)  #should print the same number
     
-    
-# Revision #1 End Date: 2/4/2022
+#Revision #3 End Date: 2/4/2022
 #End Abigail Noyes / Project #1
+#Manager:Abigail Noyes / Lead Tech: Abigail Noyes / Project #3
