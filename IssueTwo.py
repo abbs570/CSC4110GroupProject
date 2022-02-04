e2b95338a1187ef5e455d00a0b300175c3e9cc15
Abbey Noyes <abbeynoyes@gmail.com>
2022-02-04 12:53:33 -0500

    Quick Update on Issue Two
    Revision

--- IssueTwo.py
+++ IssueTwo.py
@@ -1,21 +1,21 @@
-#Revision #3 Begin Date: 2/2/2022
+#Revision #1 Begin Date: 2/4/2022
 #Begin Abigail Noyes 2/2/2020
 
-print("Group One:")
+print("Group One:") #Group One
 my_var1 = 7.0
 my_var2 = 5
-print(my_var1 % my_var2)
+print(my_var1 % my_var2) #printing group one
 
-print("Group Two:")
+print("Group Two:") #Group Two
 x = 4
 y = 5
-print(x//y)
+print(x//y) #printing group two
 
-print("Group Three:")
+print("Group Three:") #Group Three
 x=30-3**2+8//3**2*10
-print(x)
+print(x) #printing Group three
 
 
-#Revision #3 End Date: 2/4/2022
+#Revision #1 End Date: 2/4/2022
 #End Abigail Noyes / Project #1
 #Manager:Abigail Noyes / Lead Tech: Abigail Noyes / Project #3
